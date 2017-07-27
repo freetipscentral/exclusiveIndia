f7b9cc493ab4de0285efe59592ab6cb1172cca07
Dinesh Vishwakarma <VishwakarmaDinesh@johndeere.com>
2017-07-27 14:29:32 -0500

    php destinations

---
+++ letstravel_tf_v.1.1.1/upload/delhi_agra_delhi.php
@@ -0,0 +1,137 @@
+<?php include 'header.php';?>
+
+<!-- DETAIL WRAPPER -->
+<div class="detail-wrapper">
+	<div class="container">
+		<div class="detail-header">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+					<div class="detail-category color-grey-3">Delhi Agra Delhi (2 Nights)</div>
+					<h2 class="detail-title color-dark-2">delhi agra delhi</h2>
+					<div class="detail-rate rate-wrap clearfix">
+				        <div class="rate">
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+						</div>
+				      	<i></i> 
+			       	</div>
+			    </div>
+			    <div class="col-xs-12 col-sm-4">
+			    	<div class="detail-price color-dark-2">price from  <span class="color-dr-blue"> $370</span> /person</div>
+			    </div>
+	       	</div>
+       	</div>
+       	<div class="row padd-90">
+       		<div class="col-xs-12 col-md-8">
+       			<div class="detail-content color-1">
+       				<div class="detail-top slider-wth-thumbs style-2">
+						<div class="swiper-container thumbnails-preview" data-autoplay="0" data-loop="1" data-speed="500" data-center="0" data-slides-per-view="1">
+			                <div class="swiper-wrapper">
+		                    	<div class="swiper-slide active" data-val="0">
+		                    		 <img class="img-responsive img-full" src="img/agra/Taj3.jpg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="1">
+		                    		 <img class="img-responsive img-full" src="img/jaipur/amber.jpg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="2">
+		                    		 <img class="img-responsive img-full" src="img/pushkar/pushkar1.jpeg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="3">
+		                    		 <img class="img-responsive img-full" src="img/chandelao/chandelao1.jpg" alt="">		 
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="4">
+		                    		 <img class="img-responsive img-full" src="img/jodhpur/jodhpur3.jpg" alt="">           		 
+		                    	</div>
+		                    </div>
+			                <div class="pagination pagination-hidden"></div>
+			            </div>
+			            <div class="swiper-container thumbnails" data-autoplay="0" 
+			            data-loop="0" data-speed="500" data-center="0" 
+			            data-slides-per-view="responsive" data-xs-slides="3" 
+			            data-sm-slides="5" data-md-slides="5" data-lg-slides="5" 
+			            data-add-slides="5">
+			                <div class="swiper-wrapper">
+								<div class="swiper-slide current active" data-val="0">
+									<img class="img-responsive img-full" src="img/agra/Taj3.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="1">
+									<img class="img-responsive img-full" src="img/jaipur/amber.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="2">
+									<img class="img-responsive img-full" src="img/pushkar/pushkar1.jpeg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="3">
+									<img class="img-responsive img-full" src="img/chandelao/chandelao1.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="4">
+									<img class="img-responsive img-full" src="img/jodhpur/jodhpur3.jpg" alt="">
+								</div>
+							</div>
+							<div class="pagination hidden"></div>
+						</div>
+					</div>
+
+					<div class="detail-content-block">
+						<h3>General Information About tour</h3>
+						<p>Book this tour to explore visit Agra from Delhi and then return to Delhi</p>
+							
+						<h4>ITINERARY</h4>
+
+						<h5>DAY 01 DELHI</h5>
+						<p>Our representative will meet you as you EXIT the ARRIVALS TERMINAL building after which you would be transferred to your hotel.</p> 
+ 						<p>Upon arrival at the hotel, proceed for check­in at the hotel.</p>
+ 						<p>India’s capital and a major gateway to the country, contemporary Delhi is a bustling metropolis, which successfully combines in its folds ­ the ancient with the modern. Amidst the fast spiraling skyscrapers the remnants of a bygone time in the form of its many monuments stand as silent reminders to the region’s ancient legacy. The first impressions for any visitor traveling in from the airport are of a specious, garden city, tree­lined with a number of beautiful parks.</p>
+						<p>Overnight at the hotel.</p>
+
+						<h5>Day 02 DELHI - AGRA</h5>
+						<p>After buffet breakfast at the hotel, you will be driven from Delhi to Agra (203 KMS/ 4.30 Hours). Upon arrival, proceed for check­in at youAgra in terms of ambiance is still associated with its Mughal period. The Mughals besides being great rulers were also great builders and they preserved their best architectural wonders for Agra and its neighborhood.</p>
+						<p>In the afternoon, visit Taj Mahal (closed on Fridays)</p>
+						<p>The Taj Mahal is everything that has been said about it and more. Taking 22 years and 20,000 men to build, the white marble was quarried 200 miles away and was   transported   to   the   site   by   a   fleet   of   1000 elephants. Built by the Mughal Emperor Shah Jahan as   an   expression   of   his   love   for   his   wife Mumtaz Mahal, in mid-17th century, the Taj Mahal is truly one of the wonders of the world.   Though the Taj appears to be amazingly perfect from almost any angle, it is the close­up marble inlay work, which is really   astounding.   You   will   have   ample   time   to   view   and   be   mesmerized   by   this outstanding piece of architecture.</p>
+						<p>Continue to visit Agra Fort, an outstanding example of Mughal   architecture.   Agra   Fort   ­   the   seat   and   the stronghold   of   the   Mughal   Empire   under   successive generations.   This   was   the   seat   of   Mughal   rule   and administration and the present structure owes its origins to Akbar who erected the walls and gates and the first buildings on the eastern banks of Yamuna River.  Shah Jehan added the impressive quarters and the mosque while Aurangzeb added the outer ramparts.  Visit its Hall of Public Audience and its Royal Pavilions. Overnight at the hotel.</p>
+ 
+						<h5>Day 03 AGRA - DELHI</h5>
+						<p>After buffet breakfast at the hotel, you will be driven from Agra to Delhi (203 KMS / 4.30 hours).Upon arrival at Delhi, a representative will meet you and transfer you to the airport.</p>
+
+					</div>														
+				</div>       			
+       		</div>
+       		<div class="col-xs-12 col-md-4">
+       			<div class="right-sidebar">
+       				<div class="detail-block bg-dr-blue">
+       					<h4 class="color-white">price details</h4>
+       					<div class="details-desc">
+							<p class="color-grey-9">Backpacker:  <span class="color-white">USD 370</span></p>
+							<p class="color-grey-9">budget: <span class="color-white">USD 745 / person</span></p>
+							<p class="color-grey-9">comfort: <span class="color-white">USD 1100</span></p>
+							<p class="color-grey-9">luxury: <span class="color-white">USD 2200</span></p>
+							<p class="color-grey-9">rate: <span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span></p>
+							<p class="color-grey-9"><span class="color-white">The above are approximate rates and would depend on the number of travelers. In addition a * 9% Government Service Tax is applicable on your trip billing.</span></p>
+							<p class="color-grey-9">CANCELLATION: <span class="color-white">STRICT</span></p>
+						</div>
+						<div class="details-btn">
+							<a href="#" class="c-button b-40 bg-tr-1 hv-white"><span>view on map</span></a>
+							<a href="#" class="c-button b-40 bg-white hv-transparent"><span>book now</span></a>
+						</div>
+       				</div>
+
+					
+
+					<div class="sidebar-text-label bg-dr-blue color-white">useful information</div>
+
+					<div class="help-contact bg-grey-2">
+						<h4 class="color-dark-2">Need Help?</h4>
+						<p class="color-grey">If you have any queries or need more information our associates would be happy to help you.</p>
+						<a class="help-phone color-dark-2 link-dr-blue" href="tel:+91 987088698"><img src="img/detail/phone24-dark.png" alt="">+91 987088698</a>
+						<a class="help-mail color-dark-2 link-dr-blue" href="mailto:visitindia@exclusiveindiatravel.com"><img src="img/detail/letter-dark.png" alt="">go@exclusiveindiatravel.com</a>
+					</div>										      				
+       			</div>       			
+       		</div>
+       	</div>
+       	
+	</div>
+</div>
+
+    <?php include 'footer.php';?>			   
---
+++ letstravel_tf_v.1.1.1/upload/royal_rajasthan.php
@@ -0,0 +1,155 @@
+<?php include 'header.php';?>
+
+
+<!-- DETAIL WRAPPER -->
+<div class="detail-wrapper">
+	<div class="container">
+		<div class="detail-header">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+					<div class="detail-category color-grey-3">TWO-WEEK ROYAL EXPERIENCE IN RAJASTHAN</div>
+					<h2 class="detail-title color-dark-2">delhi agra jaipur pushkar chandelao jodhpur udaipur delhi</h2>
+					<div class="detail-rate rate-wrap clearfix">
+				        <div class="rate">
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+							<span class="fa fa-star color-yellow"></span>
+						</div>
+				      	<i></i> 
+			       	</div>
+			    </div>
+			    <div class="col-xs-12 col-sm-4">
+			    	<div class="detail-price color-dark-2">price from  <span class="color-dr-blue"> $370</span> /person</div>
+			    </div>
+	       	</div>
+       	</div>
+       	<div class="row padd-90">
+       		<div class="col-xs-12 col-md-8">
+       			<div class="detail-content color-1">
+       				<div class="detail-top slider-wth-thumbs style-2">
+						<div class="swiper-container thumbnails-preview" data-autoplay="0" data-loop="1" data-speed="500" data-center="0" data-slides-per-view="1">
+			                <div class="swiper-wrapper">
+		                    	<div class="swiper-slide active" data-val="0">
+		                    		 <img class="img-responsive img-full" src="img/agra/Taj3.jpg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="1">
+		                    		 <img class="img-responsive img-full" src="img/jaipur/amber.jpg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="2">
+		                    		 <img class="img-responsive img-full" src="img/pushkar/pushkar1.jpeg" alt="">
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="3">
+		                    		 <img class="img-responsive img-full" src="img/chandelao/chandelao1.jpg" alt="">		 
+		                    	</div>
+		                    	<div class="swiper-slide" data-val="4">
+		                    		 <img class="img-responsive img-full" src="img/jodhpur/jodhpur3.jpg" alt="">           		 
+		                    	</div>
+		                    </div>
+			                <div class="pagination pagination-hidden"></div>
+			            </div>
+			            <div class="swiper-container thumbnails" data-autoplay="0" 
+			            data-loop="0" data-speed="500" data-center="0" 
+			            data-slides-per-view="responsive" data-xs-slides="3" 
+			            data-sm-slides="5" data-md-slides="5" data-lg-slides="5" 
+			            data-add-slides="5">
+			                <div class="swiper-wrapper">
+								<div class="swiper-slide current active" data-val="0">
+									<img class="img-responsive img-full" src="img/agra/Taj3.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="1">
+									<img class="img-responsive img-full" src="img/jaipur/amber.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="2">
+									<img class="img-responsive img-full" src="img/pushkar/pushkar1.jpeg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="3">
+									<img class="img-responsive img-full" src="img/chandelao/chandelao1.jpg" alt="">
+								</div>
+								<div class="swiper-slide" data-val="4">
+									<img class="img-responsive img-full" src="img/jodhpur/jodhpur3.jpg" alt="">
+								</div>
+							</div>
+							<div class="pagination hidden"></div>
+						</div>
+					</div>
+
+					<div class="detail-content-block">
+						<h3>General Information About tour</h3>
+						<p>Book this tour to explore the Golden Triangle of Delhi, Agra and Jaipur and also experience more of Rajasthan in Pushkar, Jodhpur and Udaipur.</p>
+						<p>With this tour, you will enjoy the popular sights and monuments like the Red Fort in Delhi, the Taj Mahal in Agra, the Amber Fort in Jaipur and also get to experience life in India as locals do.  If you want to feel local, stay local at Homestays at some destinations enroute.</p>
+						<p>It is a typical two-weeks-travel route from Delhi to Udaipur through Agra and Rajasthan with one night outside of Jodhpur in a small village called Chandelao to get a feel of the rural life and a short break from the busy cities.</p>
+						<p>You can also participate in additional activities in each place like a slum tour in Delhi or a cooking class in Udaipur. This tour also includes lunch/dinner with a local family and visit to a school to interact with students.  This way you can meet many locals and make friends.</p>
+						<p>This trip to North India can be modified to fit any budget. Below you’ll find approximate costs for different types of travelers and an example of an itinerary. If you have a fixed time period, then we’ll add or remove destinations accordingly.</p>
+							
+						<h4>ITINERARY</h4>
+
+						<h5>Day 1 - 3 Delhi</h5>
+						<p>Delhi is the capital of India, a city rich in history and culture. Delhi is not only an interesting place to visit but is also ideally located to travel to a lot of other worthwhile places in northern India. Delhi is known for it's excellent metro service so it's very easy to get from one sight to another. There are many interesting historical sights but to get off the beaten path, you can choose a slum and a street food tour.</p> 
+ 
+						<h5>Day 3 - 4 Agra</h5>
+						<p>Agra, most popularly known for the Taj Mahal, is a favorite amongst travelers. As a key location in the story of the Mughal Empire, Agra is known for its beautiful monuments, courtyards and mosques. The Agra Fort is a lesser known but also beautiful sight you must visit during your stay in Agra. You can also explore the spice market of the old town and spend some time with a marble craftsmen, or nature walk in Shahjahan garden.</p>
+ 
+						<h5>Day 4 - 6 Jaipur</h5>
+						<p>Painted pink to welcome the Prince of Wales in 1853, Rajasthan’s capital city is chock-full with forts, palaces and temples. Shopaholics can lap up traditional footwear, textiles, crafts and gems.  You will be fascinated by the sheer variety of skills and local handicraft products and will have an opportunity to meet a bloc printer and henna artist.  Don’t miss out on delicious food in Jaipur!</p>
+
+						<h5>Day 6 - 8 Pushkar</h5>
+						<p>The pilgrimage town of Pushkar is also considered to be one of the oldest cities in India as according to legend the god Brahma himself created the town. For a deeply spiritual experience and to find out more about the traditions and rituals of Hinduism, this should be a must-visit for every Rajasthan traveler.  Chat with priests at the holy Ghats, make friends with plenty of cows and enjoy your stay at a 100 year old Haveli.</p>
+						  
+						<h5>Day 8 - 9 Chandelao</h5>
+						<p>Chandelao is a small village one hour outside of Jodhpur where rural life is still practiced the traditional way. Men ploughing fields with bulls, women in traditional attire caring water pots from the village fountain and a 200 year old Haveli turned into a heritage hotel. You enjoy watching the village women at the local art and craft project making jewellery and other handicrafts.</p>
+						 
+						 
+						<h5>Day 9 - 11  Jodhpur</h5>
+						<p>Due to its numerous blue houses in the old part of Jodhpur, this city in the dessert is often referred to as the “Blue City”. The second biggest city in Rajasthan is famous for the Mehrangarh Fort, one of the largest in India with astounding history of battles and great rulers. The old town is a labyrinth of small lanes with traditional Havelis and delicious street food.</p>
+						 
+						 
+						<h5>Day 11 - 13 Udaipur</h5>
+						<p>Udaipur has been called many things: Venice of the East, the White or Lake City and the most romantic place in India. One thing is for sure: when in Rajasthan you can’t miss this town with its white marble temples and palaces, its rooftop restaurants with perfect views of the lake as well as the beautiful surrounding landscapes.</p>
+						 
+						 
+						<h5>Day 13 - 13 Delhi</h5>
+						<p>Shopping and flight back home.</p>
+
+					</div>
+														
+				</div>       			
+       		</div>
+       		<div class="col-xs-12 col-md-4">
+       			<div class="right-sidebar">
+       				<div class="detail-block bg-dr-blue">
+       					<h4 class="color-white">price details</h4>
+       					<div class="details-desc">
+							<p class="color-grey-9">Backpacker:  <span class="color-white">USD 370</span></p>
+							<p class="color-grey-9">budget: <span class="color-white">USD 745 / person</span></p>
+							<p class="color-grey-9">comfort: <span class="color-white">USD 1100</span></p>
+							<p class="color-grey-9">luxury: <span class="color-white">USD 2200</span></p>
+							<p class="color-grey-9">rate: <span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span><span class="fa fa-star color-yellow"></span></p>
+							<p class="color-grey-9"><span class="color-white">The above are approximate rates and would depend on the number of travelers. In addition a * 9% Government Service Tax is applicable on your trip billing.</span></p>
+							<p class="color-grey-9">CANCELLATION: <span class="color-white">STRICT</span></p>
+						</div>
+						<div class="details-btn">
+							<a href="#" class="c-button b-40 bg-tr-1 hv-white"><span>view on map</span></a>
+							<a href="#" class="c-button b-40 bg-white hv-transparent"><span>book now</span></a>
+						</div>
+       				</div>
+
+					
+
+					<div class="sidebar-text-label bg-dr-blue color-white">useful information</div>
+
+					<div class="help-contact bg-grey-2">
+						<h4 class="color-dark-2">Need Help?</h4>
+						<p class="color-grey">If you have any queries or need more information our associates would be happy to help you.</p>
+						<a class="help-phone color-dark-2 link-dr-blue" href="tel:+91 987088698"><img src="img/detail/phone24-dark.png" alt="">+91 987088698</a>
+						<a class="help-mail color-dark-2 link-dr-blue" href="mailto:visitindia@exclusiveindiatravel.com"><img src="img/detail/letter-dark.png" alt="">go@exclusiveindiatravel.com</a>
+					</div>										      				
+       			</div>       			
+       		</div>
+       	</div>
+       	
+	</div>
+</div>
+    
+    <?php include 'footer.php';?>	   
